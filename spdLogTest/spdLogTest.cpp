eb45fa88563dec87ec3946e148319b04ba5a5dac
John Power <superpozitiv71@gmail.com>
2019-06-06 17:40:48 -0700

    added LoggerClass

--- spdLogTest/spdLogTest.cpp
+++ spdLogTest/spdLogTest.cpp
@@ -2,9 +2,13 @@
 
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h" 
+#include <fstream>
 
-int main()
-{
+#define DEBUGLOG
+
+
+/*
 	spdlog::info("Welcome to spdlog");
 
 	spdlog::error("Some error message with arg: {}", 1);
@@ -13,21 +17,110 @@ int main()
 	spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
 	spdlog::info("Support for floats {:03.2f}", 1.23456);
 	spdlog::info("Positional args are {1} {0}..", "too", "supported");
-	spdlog::info("{:<30}", "left aligned");
+	spdlog::info("{:<10}", "left aligned");
 
-	spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-	spdlog::debug("This message should be displayed..");
+
 
 	// change log pattern
 	spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
 
-	// Compile time log levels
-	// define SPDLOG_ACTIVE_LEVEL to desired level
+
 	SPDLOG_TRACE("Some trace message with param {}", {});
 	SPDLOG_DEBUG("Some debug message");
 
-	// Set the default logger to file logger
-	auto file_logger = spdlog::basic_logger_mt("basic_logger", "logs/basic.txt");
-	spdlog::set_default_logger(file_logger);
 	std::cout << "Hello World!\n";
+
+	*/
+
+
+class Logger {
+private:
+	
+
+public:
+	Logger(spdlog::level::level_enum lvl) {
+		spdlog::set_level(lvl);
+	}
+
+	Logger() {
+		spdlog::set_level(spdlog::level::off);
+	}
+
+	void changeLevel(spdlog::level::level_enum lvl) {
+		spdlog::set_level(lvl);
+	}
+
+	void setPattern(std::string str) {
+		spdlog::set_pattern(str);
+	}
+
+	void debug(std::string str) {
+		spdlog::debug(str.c_str());
+	}
+
+	template<typename T>
+	void error(std::string str, T arg) {
+		spdlog::error(str, arg);
+	}
+
+	template<typename T>
+	void warn(std::string str, T arg) {
+		spdlog::warn(str , arg);
+	}
+
+	template<typename T>
+	void critical(std::string str, T arg) {
+		spdlog::critical(str.c_str(), arg);
+	}
+
+	void info(std::string str) {
+		spdlog::info(str);
+	}
+
+	void createFile(std::string filePath, std::string fileName) {
+		std::ofstream file(filePath + fileName);
+		file.close();
+	}
+	
+	void createFileLogger(std::string loggerName) {
+		createFile("logs/" , "basic.txt");
+		try {
+			auto my_logger = spdlog::basic_logger_mt(loggerName, "logs/basic.txt");
+		}
+		catch (const spdlog::spdlog_ex& ex)
+		{
+			std::cout << "Log initialization failed: " << ex.what() << std::endl;
+		}
+	}
+
+
+	void createFileLoggerRotated(std::string loggerName) {
+		createFile("logs/", "rotating.txt");
+
+		try
+		{
+			//3 files 5 mb max size
+			auto file_logger = spdlog::rotating_logger_mt("file_logger", "log/rotating", 1024 * 1024 * 5, 3);
+		}
+		catch (const spdlog::spdlog_ex& ex)
+		{
+			std::cout << "Log initialization failed: " << ex.what() << std::endl;
+		}
+	}
+
+};
+
+
+int main(){ 
+
+	Logger logger(spdlog::level::debug);
+
+
+	logger.info("hello");
+
+
+	logger.debug("degug test");
+	
+	logger.critical("fatal error {}", 4);
+	logger.createFileLogger("basicLogger");
 }
