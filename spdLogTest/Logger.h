e65fb35c6202cf18bd7e0f25f875646b99e284e2
John Power <superpozitiv71@gmail.com>
2019-06-25 15:06:40 -0700

    m

--- spdLogTest/Logger.h
+++ spdLogTest/Logger.h
@@ -1,20 +1,20 @@
 #pragma once
-#include <iostream>
-#include <map>
-#include <vector>
+
 
 #define BOOST_TYPEOF_EMULATION
 
+#include <stdexcept>
+#include <string>
+#include <iostream>
 #include <boost/smart_ptr/shared_ptr.hpp>
-#include <boost/smart_ptr/make_shared_object.hpp>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include <boost/log/common.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/sources/logger.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
 
 
 #define debugInfo  __LINE__ + ' ' + __FILE__
@@ -28,9 +28,10 @@
 #define LOG_FATAL(x) BOOST_LOG_TRIVIAL(fatal) << x << debugInfo
 
 namespace logging = boost::log;
+namespace attrs = boost::log::attributes;
 namespace src = boost::log::sources;
-namespace expr = boost::log::expressions;
 namespace sinks = boost::log::sinks;
+namespace expr = boost::log::expressions;
 namespace keywords = boost::log::keywords;
 
 #endif
@@ -49,9 +50,6 @@ namespace keywords = boost::log::keywords;
 class FileLoggerBoost {
 
 	FileLoggerBoost() {
-		typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
-		boost::shared_ptr< text_sink > sink = boost::make_shared< text_sink >();
-
 	
 	}
 
--- spdLogTest/spdLogTest.cpp
+++ spdLogTest/spdLogTest.cpp
@@ -12,6 +12,16 @@ int main() {
 	
 	//logger.addFileLogger("someFile.log");
 
+
+	typedef sinks::synchronous_sink< sinks::text_file_backend > file_sink;
+	boost::shared_ptr< file_sink > sink(new file_sink(
+		keywords::file_name = "file.log",                       // file name pattern
+		keywords::target_file_name = "%Y%m%d_%H%M%S_%5N.log",   // file name pattern
+		keywords::rotation_size = 16384                         // rotation size, in characters
+	));
+
+
+
 }
 
 
